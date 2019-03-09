b081ba0c69003828dda4a0d27ddb2bf05c644320
thando mini <tzone85@gmail.com>
2019-03-09 18:40:37 +0200

    bound components, switched functions from app.js to using them in function declarations in other classes and made more event handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
@@ -15,32 +15,38 @@ class App extends Component {
     };
 
     // event Handlers tippically have suffix of Handler
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         // console.log('was clinked');
         // this.state.persons[0].name = "Mncedisi" //done mutate a ... directly react will not allow us to do this
 
         this.setState({
             persons: [
-                {name: "Mncedisi Bhovu", age: 28},
+                {name: newName, age: 28},
                 {name: "Thando", age: 32},
-                {name: "Noluthando Mapholoba    ", age: 30}
-            ] })
+                {name: "Noluthando", age: 30}
+            ]
+        })
     };
 
-  render() {
-    return (
-        <div className="App">
-            <h1>Hi, I'm a React app</h1>
-            <p>This is actually working</p>
-            <button onClick={this.switchNameHandler}>Switch Name</button>
-            <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-            <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My Hobbies: Racing</Person>
-            <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
-        </div>
-    );
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React app</h1>
+                <p>This is actually working</p>
+                <button onClick={() => this.switchNameHandler("Mncedi!!")}>Switch Name</button>
 
-      // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Does this work now'));
-  }
+                <Person name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, "Mncedisi Bhovu")}>My Hobbies: Racing</Person>
+                <Person name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}/>
+            </div>
+        );
+
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Does this work now'));
+    }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ const person  = (props) => {
     // return <p>I'm {props.name} and I'm {(Math.floor(Math.random() * 30))} years old</p>
     return (
         <div>
-            <p>I'm {props.name} and I'm {props.age} years old</p>
+            <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
             <p>{props.children}</p>
         </div>
 
