7acaddf716205ee5314e89b9c78ea69cf8fc71d8
thando mini <tzone85@gmail.com>
2019-03-09 16:30:21 +0200

    added an event handler and made the button in the es6 way

--- src/App.js
+++ src/App.js
@@ -13,12 +13,17 @@ class App extends Component {
         ]
     };
 
+    // event Handlers tippically have suffix of Handler
+    switchNameHandler = () => {
+        console.log('was clinked');
+    };
+
   render() {
     return (
         <div className="App">
             <h1>Hi, I'm a React app</h1>
             <p>This is actually working</p>
-            <button>Switch Name</button>
+            <button onClick={this.switchNameHandler}>Switch Name</button>
             <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
             <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My Hobbies: Racing</Person>
             <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
