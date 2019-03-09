a4555c5ee2d5335fddb28ed7db643909171419cf
thando mini <tzone85@gmail.com>
2019-03-09 16:10:49 +0200

    outputted children on the props as well as made use of the state property for dynamic data

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+    // this (state) can only be done in components for they extend Component
+    state = {
+        persons: [
+            {name: "Mncedi", age: 28},
+            {name: "Thando", age: 32},
+            {name: "Noluthando", age: 30}
+        ]
+    };
+
   render() {
     return (
         <div className="App">
             <h1>Hi, I'm a React app</h1>
             <p>This is actually working</p>
-            <Person name = "Mncedi" age = "28"/>
-            <Person name = "Thando" age = "32">My Hobbies: Racing</Person>
-            <Person name = "Noluthando" age = "30"/>
+            <button>Switch Name</button>
+            <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+            <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My Hobbies: Racing</Person>
+            <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
         </div>
     );
 
